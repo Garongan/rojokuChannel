5d208e1a0f84e071b152fcf91a99c3fb8ac700d9
Garongan <alvindotri@student.ub.ac.id>
2023-03-01 20:15:08 +0700

    update fitur 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -230,3 +230,92 @@ jQuery("[data-bgcolor]").each(function () {
                 .concat(range(totalPages - sideWidth + 1, totalPages));
             }
 // /product slider
+// paginasi-bawah
+$(function() {
+    // Number of items and limits the number of items per page
+    var numberOfItems = $("#info-list .content").length;
+    var limitPerPage;
+    if ($(window).width() < 576) {
+        limitPerPage = 2;
+     }
+     else {
+        limitPerPage = 4;
+    }
+    // Total pages rounded upwards
+    var totalPages = Math.ceil(numberOfItems / limitPerPage);
+    // Number of buttons at the top, not counting prev/next,
+    // but including the dotted buttons.
+    // Must be at least 5:
+    var paginationSize = 8;
+    var currentPage;
+
+    function showPage(whichPage) {
+        if (whichPage < 1 || whichPage > totalPages) return false;
+        currentPage = whichPage;
+        $("#info-list .content")
+        .hide()
+        .slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage)
+        .show();
+        // Replace the navigation items (not prev/next):
+        $(".pagination-info li").slice(1, -1).remove();
+        getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+        $("#next").before(
+            $("<li>")
+            .addClass(
+                "page-item " +
+                    (item ? "current-page " : "") +
+                    (item === currentPage ? "active " : "")
+                )
+                .append(
+                $("<a>")
+                    .addClass("page-link")
+                    .attr({
+                    href: "javascript:void(0)"
+                    })
+                    .text(item || "...")
+                )
+            )
+        });
+        return true;
+    }
+
+        // Include the prev/next buttons:
+        $(".pagination-info").append(
+            $("<li>").addClass("page-item").attr({ id: "prev" }).append(
+            $("<a>")
+                .addClass("page-link")
+                .attr({
+                href: "javascript:void(0)"
+                })
+                .text("<<")
+            ),
+            $("<li>").addClass("page-item").attr({ id: "next" }).append(
+            $("<a>")
+                .addClass("page-link")
+                .attr({
+                href: "javascript:void(0)"
+                })
+                .text(">>")
+            )
+        );
+        // Show the page links
+        $("#info-list").show();
+        showPage(1);
+
+        // Use event delegation, as these items are recreated later
+        $(document)
+        .on("click", ".pagination-info li.current-page:not(.active)", function() {
+            return showPage(+$(this).text());
+        });
+        $("#next").on("click", function() {
+            return showPage(currentPage + 1);
+        });
+
+        $("#prev").on("click", function() {
+            return showPage(currentPage - 1);
+        });
+        $(".pagination-info").on("click", function() {
+            $("html,body").animate({ scrollTop: 0 }, 0);
+        });
+    });
+// /paginasi-bawah
